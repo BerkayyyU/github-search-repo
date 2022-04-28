18a8a129e08d9f1a63fd2aeb22d88fceac15857d
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-04-28 21:06:15 +0300

    custom scroll css added

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Profile from './pages/profile';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen overfow-hidden">
-      <header className="flex justify-between py-4 fixed top-0 left-0 w-full z-50 hdx">
+    <div className="flex flex-col overflow-hidden">
+      <header className="flex justify-between py-4 fixed top-0 left-0 w-full z-50 header">
         <a href="">MVST</a>
         <a href="">Portfolio</a>
       </header>
