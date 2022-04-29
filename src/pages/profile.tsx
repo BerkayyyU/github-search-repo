f14c0f69618f9ecba9d8122ca7416e8fa82a745b
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-04-29 03:12:01 +0300

    loading spinner added

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Repository, { RepositoryModel } from '../components/repository/repository';
 import User, { UserModel } from '../components/user/user';
+import Loading from '../ui/Loading';
 import styles from './profile.module.css';
 
 const Profile = () => {
@@ -9,6 +10,7 @@ const Profile = () => {
   const [filteredRepos, searchRepos] = useState<RepositoryModel[]>([]);
 
   const [user, setUser] = useState<UserModel | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const githubName = 'berkayyyU';
   const githubApi = 'https://api.github.com/';
@@ -19,55 +21,74 @@ const Profile = () => {
   }
 
   useEffect(() => {
+    // fetchUser();
+    // fetchUserRepos();
+  }, []);
+
+  const fetchUser = async () => {
+    setLoading(true);
+    await fetch(`${githubApi}users/${githubName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  };
+
+  const fetchUserRepos = async () => {
+    setLoading(true);
     fetch(`${githubApi}users/${githubName}/repos`)
       .then((response) => response.json())
       .then((data) => {
         setRepos(data);
         searchRepos(data);
+        setLoading(false);
       });
-    fetch(`${githubApi}users/${githubName}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setUser(data);
-      });
-  }, []);
+  };
+
   return (
-    <div className="flex flex-col md:flex-row mx-4 sm:mx-8 mt-20 sm:mt-24">
-      {user ? (
-        <User
-          id={user.id}
-          avatar_url={user?.avatar_url}
-          html_url={user?.html_url}
-          login={user?.login}
-          name={user.name}
-          followers={user.followers}
-          following={user.following}
-        />
-      ) : null}
-      <div className="w-full flex flex-col items-center md:ml-8">
-        <input type="text" onChange={handleChange} placeholder="Find a repository..." className={styles.search} />
-        <ul className="h-full w-full overflow-auto">
-          {filteredRepos.length > 0 ? (
-            filteredRepos.map((repo: RepositoryModel) => {
-              return (
-                <Repository
-                  key={repo.id}
-                  id={repo.id}
-                  html_url={repo.html_url}
-                  name={repo.name}
-                  visibility={repo.visibility}
-                  description={repo.description}
-                  language={repo.language}
-                  updated_at={repo.updated_at}
-                  stargazers_count={repo.stargazers_count}
-                />
-              );
-            })
-          ) : (
-            <p className="w-full mt-4 text-center">No matching repositories.</p>
-          )}
-        </ul>
-      </div>
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="flex flex-col md:flex-row mx-4 sm:mx-8 mt-20 sm:mt-24">
+          {user ? (
+            <User
+              id={user.id}
+              avatar_url={user?.avatar_url}
+              html_url={user?.html_url}
+              login={user?.login}
+              name={user.name}
+              followers={user.followers}
+              following={user.following}
+            />
+          ) : null}
+          <div className="w-full flex flex-col items-center md:ml-8">
+            <input type="text" onChange={handleChange} placeholder="Find a repository..." className={styles.search} />
+            <ul className="h-full w-full overflow-auto">
+              {filteredRepos.length > 0 ? (
+                filteredRepos.map((repo: RepositoryModel) => {
+                  return (
+                    <Repository
+                      key={repo.id}
+                      id={repo.id}
+                      html_url={repo.html_url}
+                      name={repo.name}
+                      visibility={repo.visibility}
+                      description={repo.description}
+                      language={repo.language}
+                      updated_at={repo.updated_at}
+                      stargazers_count={repo.stargazers_count}
+                    />
+                  );
+                })
+              ) : (
+                <p className="w-full mt-4 text-center">No matching repositories.</p>
+              )}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/ui/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;
